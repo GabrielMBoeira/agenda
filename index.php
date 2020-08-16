dc23e0deb7f8b4c97d12517cf2de4eaf4baa1a1d
GabrielMBoeira <gboeira777@gmail.com>
2020-08-16 20:21:57 -0300

    ajusanto url amigável index.php

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <?php
-$uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+$url = (isset($_GET['url']) ? $_GET['url'] : '');
+$url = array_filter(explode('/', $url)); 
 
-if($uri === '/' || $uri === '' ||  $uri === '/index.php' || $uri ==='/agenda/') {
-    $uri = 'agenda.php';
+$file_url = $url[0].'.php';
+$file_path = 'src/view/'.$file_url;
+
+if (is_file($file_path)) {
+    require_once($file_path);
+} else {
+    require_once('src/view/404.php');
 }
+?>
 
-require_once('src/view'."/{$uri}");
+
