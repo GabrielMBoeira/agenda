cf9a4894f2011282ebffabb0146d3de2433e2d86
GabrielMBoeira <gboeira777@gmail.com>
2020-08-16 10:47:28 -0300

    page agenda.php

--- src/view/agenda.php
+++ src/view/agenda.php
@@ -20,7 +20,7 @@
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                     <ul class="navbar-nav">
                         <li class="nav-item active">
-                            <a class="nav-link" href="#">Compromissos <span class="sr-only">(current)</span></a>
+                            <a class="nav-link" href="#">Cadastrar compromissos<span class="sr-only">(current)</span></a>
                         </li>
                     </ul>
                 </div>
@@ -28,52 +28,66 @@
         </div>
     </header>
 
-    <main class="main">
+    <main class="main p-4">
         <div class="container-fluid">
-
             <div class="row">
-                <div class="col-md-4">
+                <div class="col-md-4 mt-3">
                     <div class="card">
                         <h5 class="card-header">
-                            Card title
+                            Compromisso
+                            <div class="user-date">
+                                Usuário: Gabriel - Data limite: 12/01/2021 - 16:00:00
+                            </div>
                         </h5>
                         <div class="card-body">
                             <p class="card-text">
-                                Card content
+                                Agenda: Limpar nomes
                             </p>
                         </div>
-                        <div class="card-footer">
-                            Card footer
+                        <div class="card-footer d-flex justify-content-center">
+                            <button class="btn btn-danger m-1">Deletar</button>
+                            <button class="btn btn-success m-1">Alterar</button>
+                            <button class="btn btn-primary m-1">Concluído</button>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-4 mt-3">
                     <div class="card">
                         <h5 class="card-header">
-                            Card title
+                            Compromisso
+                            <div class="user-date">
+                                Usuário: Gabriel - Data limite: 12/01/2021 - 16:00:00
+                            </div>
                         </h5>
                         <div class="card-body">
                             <p class="card-text">
-                                Card content
+                                Agenda: Limpar nomes
                             </p>
                         </div>
-                        <div class="card-footer">
-                            Card footer
+                        <div class="card-footer d-flex justify-content-center">
+                            <button class="btn btn-danger m-1">Deletar</button>
+                            <button class="btn btn-success m-1">Alterar</button>
+                            <button class="btn btn-primary m-1">Concluído</button>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-4 mt-3">
                     <div class="card">
                         <h5 class="card-header">
-                            Card title
+                            Compromisso
+                            <div class="user-date">
+                                Usuário: Gabriel - Data limite: 12/01/2021 - 16:00:00
+                            </div>
                         </h5>
                         <div class="card-body">
                             <p class="card-text">
-                                Card content
+                                Agenda: Limpar nomes
                             </p>
                         </div>
-                        <div class="card-footer">
-                            Card footer
+                        <div class="card-footer d-flex justify-content-center">
+                            <button class="btn btn-danger m-1">Deletar</button>
+                            <button class="btn btn-success m-1">Alterar</button>
+                            <button class="btn btn-primary m-1">Concluído</button>
                         </div>
                     </div>
                 </div>
@@ -81,7 +95,7 @@
         </div>
     </main>
 
-    <footer class="footer d-flex justify-content-end bg-dark text-white">
+    <footer class="footer d-flex justify-content-end bg-dark text-white p-3">
         Agenda Gabriel & Ana
     </footer>
 
