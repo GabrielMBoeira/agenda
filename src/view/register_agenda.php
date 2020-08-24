a27bb1c9d35d3e7c40d03c84bc19d86bece4c9f5
GabrielMBoeira <gboeira777@gmail.com>
2020-08-23 23:34:37 -0300

    insert in database

--- src/view/register_agenda.php
+++ src/view/register_agenda.php
@@ -2,18 +2,38 @@
 require_once('template/header.php');
 require_once(dirname(__FILE__, 2) . '/db/Connection.php');
 
-$conn = Connection::connectionDB();
+
+if (isset($_POST['save'])) {
+
+    $appointment = $_POST['appointment'];
+
+
+    $query = "INSERT INTO note (appointment) VALUES ('$appointment')";
+
+    $conn = Connection::connectionDB();
+
+    if (pg_query($query)) {
+        $msg[0] = '<div class="alert alert-success" role="alert">Cadastrado com sucesso!</div>';
+        unset($_POST);
+    } else {
+        $msg[0] = '<div class="alert alert-danger" role="alert">Manutenção não cadastrada!</div>';
+    }
+}
+
+
+
 ?>
 
 <main class="main p-4">
     <div class="container">
+        <?= $msg[0]; ?>
         <form action="#" method="POST">
             <div class="form-row mt-3">
                 <div class="form-group col-md-12">
                     <label for="appointment">
                         Compromisso
                     </label>
-                    <input type="text" class="form-control" id="appointment" />
+                    <input type="text" class="form-control" id="appointment" name="appointment" />
                 </div>
                 <div class="form-group col-md-6">
                     <label for="date">
@@ -21,8 +41,8 @@ $conn = Connection::connectionDB();
                     </label>
                     <input type="date" class="form-control" id="date" />
                     <div class="d-flex justify-content-start">
-                        <button type="submit" class="btn btn-primary mt-3">
-                            Cadastrar
+                        <button type="submit" class="btn btn-primary mt-3" name="save">
+                            Salvar
                         </button>
                     </div>
                 </div>
