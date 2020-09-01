b8e231e311b828ae507e99dc037a4685f1fe1775
GabrielMBoeira <gboeira777@gmail.com>
2020-09-01 01:26:38 -0300

    concluindo page agenda

--- src/view/agenda.php
+++ src/view/agenda.php
@@ -6,14 +6,14 @@ $conn = Connection::connectionDB();
 
     $registros = [];
     
-    $resultado = pg_query($conn, "SELECT * FROM note;");
+    $resultado = pg_query($conn, "SELECT * FROM note ORDER BY appointment_date ASC ;");
 
     if  (!$resultado) {
-        echo "Não executou";
+        echo "Erro na Query";
     }
 
     if (pg_num_rows($resultado) == 0) {
-        echo "0 records";
+        echo "pg 0 records";
     } else {
         while ($row = pg_fetch_assoc($resultado)) {
                 $registros[] = $row;
@@ -23,46 +23,37 @@ $conn = Connection::connectionDB();
 ?>
 
 <main class="main p-4">
-    <table>
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>NOTE</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($registros as $registro):?>
-                <tr>
-                    <td><?= $registro['id'] ?></td>
-                    <td><?= $registro['appointment'] ?></td>
-                </tr>
-            <?php endforeach ?>
-        </tbody>
-    </table>
-
     <div class="container-fluid">
         <div class="row">
+            <?php foreach ($registros as $registro): ?>
             <div class="col-md-4 mt-3">
                 <div class="card">
                     <h5 class="card-header">
                         Compromisso
-                        <div class="user-date">
-                            Usuário: Gabriel - 
-                            <span class="date-appointment">Data: 12/01/2021</span> 
+                        <div class="user-date mt-2">
+                            <span class="user-text">Usuário: <?= $registro['login'] ?> - Data:</span> 
+                            <span class="date-appointment">
+                                <?php 
+                                $data = $registro['appointment_date']; 
+                                $dtFormat = strtotime($data);
+                                echo date('d/m/Y', $dtFormat);
+                                ?>
+                            </span> 
                         </div>
                     </h5>
                     <div class="card-body">
                         <p class="card-text">
-                            Agenda: Limpar nomes
+                            Agenda: <?= $registro['appointment'] ?>
                         </p>
                     </div>
                     <div class="card-footer d-flex justify-content-center">
-                        <button class="btn btn-danger m-1">Deletar</button>
-                        <button class="btn btn-success m-1">Alterar</button>
-                        <button class="btn btn-primary m-1">Concluído</button>
+                        <button class="btn btn-danger btn-sm m-1">Deletar</button>
+                        <button class="btn btn-success btn-sm m-1">Alterar</button>
+                        <button class="btn btn-primary btn-sm m-1">Concluído</button>
                     </div>
                 </div>
             </div>
+            <?php endforeach ?>
         </div>
     </div>
 </main>
--- src/view/register_agenda.php
+++ src/view/register_agenda.php
@@ -4,22 +4,39 @@ require_once(dirname(__FILE__, 2) . '/db/Connection.php');
 
 //Iniciando array vazio para inicialização da página
 $msg[0] = '';
+$erros = [];
 
 if (isset($_POST['save'])) {
 
     $appointment = htmlspecialchars($_POST['appointment'], ENT_QUOTES);
-    
-    $query = "INSERT INTO note (appointment) VALUES ('$appointment')";
+    $login = htmlspecialchars($_POST['login'], ENT_QUOTES);
+    $appointment_date = htmlspecialchars($_POST['appointment_date'], ENT_QUOTES);
 
-    $conn = Connection::connectionDB();
-
-    if (pg_query($query)) {
-        $msg[0] = '<div class="alert alert-success" role="alert">Cadastrado com sucesso!</div>';
-        unset($_POST);
-    } else {
-        $msg[0] = '<div class="alert alert-danger" role="alert">Manutenção não cadastrada!</div>';
+    if (trim($appointment) === '') {
+        $erros['appointment'] = 'Compromisso é obrigatório';
     }
-    pg_close();
+
+    if (trim($login) === 'Selecione') {
+        $erros['login'] = 'Favor adicionar responsável';
+    }
+
+    if (trim($appointment_date) === '') {
+        $erros['appointment_date'] = 'Favor adicionar data do compromisso';
+    }
+
+    if (!$erros) {
+        $query = "INSERT INTO note (appointment, login, appointment_date) VALUES ('$appointment', '$login', '$appointment_date')";
+
+        $conn = Connection::connectionDB();
+
+        if (pg_query($query)) {
+            $msg[0] = '<div class="alert alert-success" role="alert">Cadastrado com sucesso!</div>';
+            unset($_POST);
+        } else {
+            $msg[0] = '<div class="alert alert-danger" role="alert"> não cadastrada!</div>';
+        }
+        pg_close();
+    }
 }
 
 ?>
@@ -31,20 +48,33 @@ if (isset($_POST['save'])) {
             <div class="form-row mt-3">
                 <div class="form-group col-md-6">
                     <label for="appointment">
-                        Compromisso
+                        <strong>Compromisso</strong>
                     </label>
-                    <input type="text" class="form-control" id="appointment" name="appointment" />
+                    <input type="text" class="form-control <?= $erros['appointment'] ? 'is-invalid' : '' ?>" id="appointment" name="appointment" />
+                    <div class="invalid-feedback"><?= $erros['appointment'] ?></div>
                 </div>
                 <div class="form-group col-md-6">
-                    <label for="date">
-                        Date
+                    <label for="appointment_date">
+                        <strong>Data</strong>
                     </label>
-                    <input type="date" class="form-control" id="date" name="date" />
-                    <div class="d-flex justify-content-end">
-                        <button type="submit" class="btn btn-primary mt-3" name="save">
-                            Salvar
-                        </button>
-                    </div>
+                    <input type="date" class="form-control <?= $erros['appointment_date'] ? 'is-invalid' : '' ?>" id="appointment_date" name="appointment_date" />
+                    <div class="invalid-feedback"><?= $erros['appointment_date'] ?></div>
+                </div>
+                <div class="form-group col-12">
+                    <label for="login">
+                        <strong>Responsável</strong>
+                    </label>
+                    <select name="login" id="login" class="form-control <?= $erros['login'] ? 'is-invalid' : '' ?>">
+                        <option value="Selecione">Selecione</option>
+                        <option value="Gabriel">Gabriel</option>
+                        <option value="Ana Clara">Ana Clara</option>
+                    </select>
+                    <div class="invalid-feedback"><?= $erros['login'] ?></div>
+                </div>
+                <div class="d-flex justify-content-end col-12">
+                    <button type="submit" class="btn btn-primary mt-3" name="save">
+                        Salvar
+                    </button>
                 </div>
             </div>
         </form>
